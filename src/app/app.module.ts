f9878a37d0e53403da069c853d1b5e3728b19da4
Michele Akeson <akesonm@mcmaster.ca>
2022-10-03 11:53:51 -0400

    added some basic material.io

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,11 @@ import { ShapeFillComponent } from './components/shape-fill/shape-fill.component
 import { ManipulativeStudioComponent } from './components/manipulative-studio/manipulative-studio.component';
 import { ClipTemplateComponent } from './components/clip-template/clip-template.component';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,7 +21,13 @@ import { FormsModule } from '@angular/forms';
     ClipTemplateComponent
   ],
   imports: [
-    BrowserModule, FormsModule
+    BrowserModule, 
+    FormsModule, 
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatInputModule,
+    MatButtonModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
